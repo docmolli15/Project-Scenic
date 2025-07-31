5c560fa5df00cf6814d99c582c27c5b5da2e256c
docmolli15 <46311131+docmolli15@users.noreply.github.com>
2025-07-30 21:02:32 -0700

    Velocity

---
+++ scenes/player.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+#ADD ELEMENT TO THE INSPECTOR THAT WILL BE USED TO GOVERN THE SPEED OF THE LANDSCAPE AND TRAIN
+@export var velocity: int
+#VARIABLES TO MANIPULATE THE TRAIN ANIMATION AND SMOKE
+@onready var trainAnim = %AnimatedSprite2D
+@onready var smokeStack = %GPUParticles2D
+
+func _ready() -> void:
+	pass
+	
+func _process(_delta: float) -> void:
+	trainAnimation()
+	smoke()
+
+func smoke():
+	smokeStack.amount_ratio = velocity * 0.03
+
+func trainAnimation():
+	trainAnim.speed_scale = velocity * .12
--- scripts/environment/world.gd
+++ scripts/environment/world.gd
@@ -1,5 +1,4 @@
 extends Node2D
-
 #ADD ELEMENT TO THE INSPECTOR THAT WILL BE USED TO GOVERN THE SPEED OF THE LANDSCAPE AND TRAIN
 @export var velocity: int
 #VARIABLE TO ACCESS PROPERTIES OF THE PARALLAX2D NODES
@@ -8,13 +7,17 @@ func _ready() -> void:
 	pass
 	
 func _process(_delta: float) -> void:
-	scrollSpeed()
+	animationSpeed()
 
-func scrollSpeed():
+func animationSpeed():
 	#VARIABLE TO ACCESS PROPERTIES OF THE PARALLAX2D NODES
 	var childrenNodes = get_children()
+	#AUTOSCROLL VARIABLE THAT INCREASES AS FUNCTION ITERATES THROUGH LAYERS
+	var base = -2
 	for child in childrenNodes:
 		#IDENTIFY CHILDREN OF THE WORLD NODE THAT ARE PARALLAX2DS
 		if child is Parallax2D:
 			#CONTROL SPEED THROUGH AUTOSCROLL
-			child.autoscroll.x *= 1.2 * velocity
+			child.autoscroll.x = base * velocity
+			#INCREASE BASE WITH EACH LAYER
+			base -= 2
