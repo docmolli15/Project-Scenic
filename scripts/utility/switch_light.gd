273a5f81a58fc3fe0b50c123c5a77b55af39d320
docmolli15 <46311131+docmolli15@users.noreply.github.com>
2025-08-15 12:59:25 -0700

    Experimented with lights

--- scripts/utility/switch_light.gd
+++ scripts/utility/switch_light.gd
@@ -2,8 +2,13 @@ extends Area2D
 
 @onready var light = %PointLight2D
 
-func _turn_off(_body):
-	light.enabled = false
-
-func _turn_on(_body):
+func _ready() -> void:
 	light.enabled = true
+
+func _turn_off(area):
+	if area.name == "DarkZone":
+		light.enabled = false
+
+func _turn_on(area):
+	if area.name == "DarkZone":
+		light.enabled = true
